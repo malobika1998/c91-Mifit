244632726d369f203b5defe7df05ecdc1b1fd79c
goru155 <74966689+goru155@users.noreply.github.com>
2022-05-31 16:32:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAlE0IHYGx2hYOdAD3fhAbH1MrT2idWQZs",
+    authDomain: "fitness-app-66b46.firebaseapp.com",
+    projectId: "fitness-app-66b46",
+    storageBucket: "fitness-app-66b46.appspot.com",
+    messagingSenderId: "178136904757",
+    appId: "1:178136904757:web:0b5135727cff56b83d2d7c"
+    };
