aed277d09e210ebbd1144bfeeca3e6f757a2abcf
goru155 <74966689+goru155@users.noreply.github.com>
2022-05-31 19:24:29 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,20 @@
-import * as React from 'react';
-import { createSwitchNavigator, createAppContainer } from "react-navigation";
-
-import LoginScreen from "./screens/LoginScreen";
-import LoadingScreen from "./screens/LoadingScreen";
-
-import * as firebase from "firebase";
-import { firebaseConfig } from "./config";
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-} else {
-  firebase.app();
-}
-
-const AppSwitchNavigator = createSwitchNavigator({
-  LoadingScreen: LoadingScreen,
-  LoginScreen: LoginScreen,
-})
-
-const AppNavigator = createAppContainer(AppSwitchNavigator)
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <AppNavigator />
-  )
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
